9a09e153767cecdf72be2689b7923cfa6fb6f0b7
EC2 Default User <ec2-user@ip-172-31-35-78.ap-northeast-1.compute.internal>
2023-08-10 17:32:04 +0900

    [finish]log-in&sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  
+  private
+    def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+        admin_posts_path
+      else
+        posts_path
+      end
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :user
+        root_path
+      elsif resource_or_scope == :admin
+        new_admin_session_path
+      else
+        root_path
+      end
+    end
+
 end
