c6ddaa302d7b4e7003279b2594ac6d0ad4efe0ad
EC2 Default User <ec2-user@ip-172-31-35-78.ap-northeast-1.compute.internal>
2023-08-13 12:59:33 +0900

    [add]post_function

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,56 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_end_user!
+  before_action :ensure_correct_end_user, only: [:edit, :update, :destroy]
+
+  def new
+    @post = Post.new
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.end_user_id = current_end_user.id
+    if @post.save
+      redirect_to post_path(@post), notice: "投稿が成功しました"
+    else
+      @post = Post.new
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "投稿を更新しました"
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def ensure_correct_end_user
+    @post = Post.find(params[:id])
+    unless @post.end_user == current_end_user
+      redirect_to posts_path
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :end_user
   has_many :post_images,dependent: :destroy
-  has_many :fovorite_posts,dependent: :destroy
+  # has_many :fovorite_posts,dependent: :destroy
   has_many :comments,dependent: :destroy
   has_many :post_and_tags,dependent: :destroy
   
@@ -10,4 +10,6 @@ class Post < ApplicationRecord
      validates :title
      validates :body
   end
+  validates :title,length:{maximum:50}
+  validates :body,length:{maximum:300}
 end
