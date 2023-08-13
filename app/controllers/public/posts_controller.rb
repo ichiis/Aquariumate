6e55eb4f07246d027a69182410180aab2a1c0c2a
EC2 Default User <ec2-user@ip-172-31-35-78.ap-northeast-1.compute.internal>
2023-08-13 14:19:31 +0900

    [add]tag_function

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -8,6 +8,8 @@ class Public::PostsController < ApplicationController
   
   def show
     @post = Post.find(params[:id])
+    @tag_list = @post.tags.pluck(:tag_name).join(',')
+    @post_and_tags = @post.tags
   end
   
   def index
@@ -17,7 +19,9 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.end_user_id = current_end_user.id
+    tag_list = params[:post][:tag_name].split(',')
     if @post.save
+      @post.save_tags(tag_list)
       redirect_to post_path(@post), notice: "投稿が成功しました"
     else
       @post = Post.new
@@ -26,10 +30,13 @@ class Public::PostsController < ApplicationController
   end
   
   def edit
+    @tag_list = @post.tags.pluck(:tag_name).join(',')
   end
   
   def update
+    tag_list=params[:post][:tag_name].split(',')
     if @post.update(post_params)
+      @post.save_tags(tag_list)
       redirect_to post_path(@post), notice: "投稿を更新しました"
     else
       render "edit"
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   # has_many :fovorite_posts,dependent: :destroy
   has_many :comments,dependent: :destroy
   has_many :post_and_tags,dependent: :destroy
+  has_many :tags, through: :post_and_tags
   
   with_options presence: true do
      validates :end_user_id
@@ -12,4 +13,22 @@ class Post < ApplicationRecord
   end
   validates :title,length:{maximum:50}
   validates :body,length:{maximum:300}
+  
+  #タグの保存
+  def save_tags(tags)
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    # 古いタグを消す
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(tag_name:old_name)
+    end
+
+    # 新しいタグを保存
+    new_tags.each do |new_name|
+      tag = Tag.find_or_create_by(tag_name:new_name)
+      self.tags << tag
+    end
+  end
 end
